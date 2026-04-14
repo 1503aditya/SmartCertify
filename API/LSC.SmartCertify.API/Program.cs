b775ae968bce82f24d369e05b71eeb1a2628658b
Aditya Narayan Upadhyay <adityanupadhyay@gmail.com>
2026-04-14 23:54:16 +0530

    Moved from Azure and removed older files

---
+++ API/LSC.SmartCertify.API/Program.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using LSC.SmartCertify.API.Filters;
+using LSC.SmartCertify.Application;
+//using LSC.SmartCertify.Application.DTOValidations;
+//using LSC.SmartCertify.Application.Interfaces.Courses;
+//using LSC.SmartCertify.Application.Services;
+using LSC.SmartCertify.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers( options =>
+{
+    options.Filters.Add<ValidationFilter>();
+}).ConfigureApiBehaviorOptions(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// ✅ AutoMapper fix
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
---
+++ API/LSC.SmartCertify.Application/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using LSC.SmartCertify.Application.DTOs;
+using LSC.SmartCertify.Domain.EntitiesNew;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSC.SmartCertify.Application
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<CreateCourseDto, Course>();
+            CreateMap<UpdateCourseDto, Course>();
+            CreateMap<Question, QuestionDto>().ReverseMap();
+            CreateMap<CreateQuestionDto, Question>();
+            CreateMap<UpdateQuestionDto, Question>();
+        }
+    }
+}
